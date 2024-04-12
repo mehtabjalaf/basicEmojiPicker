849fea55fb2b0544c3837907656c3a0038f5a6d7
Mehtab Singh Jalaf <77369363+mehtabjalaf@users.noreply.github.com>
2024-04-11 21:22:59 -0400

    Initial Commit

---
+++ try3/try3App.swift
@@ -0,0 +1,17 @@
+//
+//  try3App.swift
+//  try3
+//
+//  Created by Mehtab Jalaf on 2024-04-11.
+//
+
+import SwiftUI
+
+@main
+struct try3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
